46130a759b9a97d8b587735f8cb49c8620faa917
bayu07750 <bayu07750@gmail.com>
2022-02-01 08:28:32 +0700

    create MainActivityScreen

--- app/src/main/java/com/bayu/dailytask/MainActivity.kt
+++ app/src/main/java/com/bayu/dailytask/MainActivity.kt
@@ -3,21 +3,25 @@ package com.bayu.dailytask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.bayu.dailytask.task.TaskScreen
+import com.bayu.dailytask.task.TaskViewModel
 import com.bayu.dailytask.ui.theme.DailyTaskTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: TaskViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DailyTaskTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainActivityScreen(viewModel)
                 }
             }
         }
@@ -25,14 +29,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DailyTaskTheme {
-        Greeting("Android")
-    }
+private fun MainActivityScreen(viewModel: TaskViewModel) {
+    TaskScreen(
+        tasks = viewModel.tasks,
+        onAddTask = viewModel::onAddTask,
+    )
 }
