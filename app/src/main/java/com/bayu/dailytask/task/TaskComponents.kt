23529a07bd3234597c3968bd765fc014461f7893
bayu07750 <bayu07750@gmail.com>
2022-02-01 08:23:08 +0700

    create TaskScreen and TaskComponents

---
+++ app/src/main/java/com/bayu/dailytask/task/TaskComponents.kt
@@ -0,0 +1,182 @@
+package com.bayu.dailytask.task
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Mood
+import androidx.compose.material.icons.rounded.PushPin
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Tasks(
+    items: List<Task>,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(top = 16.dp)
+    ) {
+        items(items) { item ->
+            TaskItem(item = item)
+        }
+    }
+}
+
+@Composable
+fun TaskItem(
+    item: Task,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 2.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1F),
+            ) {
+                Text(
+                    text = item.title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Text(
+                    text = item.description,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.body1,
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = if (item.isImportant) Icons.Rounded.PushPin else Icons.Rounded.Mood,
+                    contentDescription = "Pin"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskForms(
+    title: String,
+    onTitleChange: (String) -> Unit,
+    desc: String,
+    onDescChange: (String) -> Unit,
+    isImportant: Boolean,
+    onIsImportantChange: (Boolean) -> Unit,
+    buttonSlots: CompFunType,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+    ) {
+        OutlinedTaskForm(
+            text = title,
+            onTextChange = onTitleChange,
+            singleLine = true,
+        ) {
+            Text(text = "Title")
+        }
+        OutlinedTaskForm(
+            text = desc,
+            onTextChange = onDescChange
+        ) {
+            Text(text = "Description (optional)")
+        }
+        TaskCheckbox(
+            taskIsImportant = isImportant,
+            setTaskIsImportant = onIsImportantChange
+        ) {
+            Text(text = "Important?")
+        }
+        buttonSlots()
+    }
+}
+
+@Composable
+fun OutlinedTaskForm(
+    text: String,
+    onTextChange: (String) -> Unit,
+    singleLine: Boolean = false,
+    label: CompFunType,
+) {
+    OutlinedTextField(
+        value = text,
+        onValueChange = onTextChange,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        label = label,
+        singleLine = singleLine,
+    )
+}
+
+@Composable
+fun TaskCheckbox(
+    taskIsImportant: Boolean,
+    setTaskIsImportant: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    label: CompFunType,
+) {
+    Row(
+        modifier = modifier
+            .padding(bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Checkbox(
+            checked = taskIsImportant,
+            onCheckedChange = setTaskIsImportant
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        label()
+    }
+}
+
+@Composable
+fun TaskIconButton(
+    imageVector: ImageVector,
+    contentDescription: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Icon(imageVector = imageVector, contentDescription = contentDescription)
+    }
+}
+
+@Composable
+fun AddTaskFab(
+    onClick: () -> Unit,
+) {
+    FloatingActionButton(
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Add,
+            contentDescription = "Add Task"
+        )
+    }
+}
---
+++ app/src/main/java/com/bayu/dailytask/task/TaskScreen.kt
@@ -0,0 +1,137 @@
+package com.bayu.dailytask.task
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+typealias CompFunType = @Composable () -> Unit
+
+@Composable
+fun TaskScreen(
+    tasks: List<Task>,
+    onAddTask: (Task) -> Unit,
+) {
+    val (isActionAdd, setIsActionAdd) = rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TaskTopBar(isActionAdd = isActionAdd, onIsActionAddChange = setIsActionAdd)
+        },
+        floatingActionButton = {
+            if (!isActionAdd) {
+                AddTaskFab {
+                    setIsActionAdd(true)
+                }
+            } else {
+                Box { }
+            }
+        },
+    ) {
+        TaskScreenContent(
+            tasks = tasks,
+            onAddTask = onAddTask,
+            isActionAdd = isActionAdd,
+            onIsActionAddChange = setIsActionAdd,
+            modifier = Modifier.padding(it)
+        )
+    }
+}
+
+@Composable
+fun TaskTopBar(
+    isActionAdd: Boolean,
+    onIsActionAddChange: (Boolean) -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = if (isActionAdd) "Add" else "Daily Tasks",
+                style = MaterialTheme.typography.h6
+            )
+        },
+        navigationIcon = {
+            if (isActionAdd) {
+                TaskIconButton(
+                    imageVector = Icons.Rounded.ArrowBack,
+                    contentDescription = "Back",
+                    onClick = { onIsActionAddChange(false) },
+                )
+            } else {
+                TaskIconButton(
+                    imageVector = Icons.Rounded.Menu,
+                    contentDescription = "Menu",
+                    onClick = { /* TODO */ }
+                )
+            }
+        },
+    )
+}
+
+@Composable
+fun TaskScreenContent(
+    tasks: List<Task>,
+    onAddTask: (Task) -> Unit,
+    isActionAdd: Boolean,
+    onIsActionAddChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val (taskTitle, setTaskTitle) = rememberSaveable { mutableStateOf("") }
+    val (taskDesc, setTaskDesc) = rememberSaveable { mutableStateOf("") }
+    val (taskIsImportant, setTaskIsImportant) = rememberSaveable { mutableStateOf(false) }
+
+    val newTaskAction = {
+        val task = Task(taskTitle, taskDesc, taskIsImportant)
+        onAddTask(task)
+        setTaskTitle("")
+        setTaskDesc("")
+        setTaskIsImportant(false)
+        onIsActionAddChange(false)
+    }
+
+    Surface(modifier = modifier) {
+        if (isActionAdd) {
+            TaskForms(
+                title = taskTitle,
+                onTitleChange = setTaskTitle,
+                desc = taskDesc,
+                onDescChange = setTaskDesc,
+                isImportant = taskIsImportant,
+                onIsImportantChange = setTaskIsImportant,
+            ) {
+                Button(
+                    onClick = newTaskAction,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                ) {
+                    Text(text = "Add Task")
+                }
+            }
+        } else {
+            if (tasks.isNotEmpty()) {
+                Tasks(items = tasks)
+            } else {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = "No Task added yet!",
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
+            }
+        }
+    }
+}
